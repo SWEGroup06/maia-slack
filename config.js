55da196ff77ff5373aa11b2de13b607ef5a09561
KPal81 <kpal81xd@gmail.com>
2020-10-22 19:52:29 +0100

    added dotenv and cleaned up file structure

--- config.js
+++ config.js
@@ -3,13 +3,13 @@ const fs = require('fs');
 const ENV = fs.existsSync('./.env.json') ? require('./.env.json') : process.env;
 
 module.exports = {
-    prefix: "maia",
+    prefix: 'maia',
 
     admins: [
-        "U01CEG3V7B7", //KPal
+        'U01CEG3V7B7', // KPal
     ],
 
     BOT_TOKEN: ENV.BOT_TOKEN,
 
     serverURL: 'https://maia-server.herokuapp.com',
-}
+};
--- index.js
+++ index.js
@@ -1,72 +1,75 @@
 const SlackClient = require('@slack/client');
-const { RTMClient, WebClient  } = SlackClient;
+const { RTMClient, WebClient } = SlackClient;
 
-const CONFIG = require('./config.js');
+// Load environment variables
+require('dotenv').config();
 
 // Slack Interfaces
-const rtm = new RTMClient(CONFIG.BOT_TOKEN);
-const web = new WebClient(CONFIG.BOT_TOKEN);
+const rtm = new RTMClient(process.env.MAIA_BOT_TOKEN);
+const web = new WebClient(process.env.MAIA_BOT_TOKEN);
 
-const COMMANDS = require('./commands.js')(CONFIG, web);
+const CONFIG = require('./config.js');
+const COMMANDS = require('./lib/commands.js')(CONFIG, web);
 
 
 // Start callback
-rtm.on('ready', function() {
-    console.log("============================")
-    console.log(`BOT is ONLINE`);
-    console.log("============================")
+rtm.on('ready', function () {
+  console.log('============================');
+  console.log(`Maia is ONLINE`);
+  console.log('============================');
 });
 
 
-// Message Event callback 
+// Message Event callback
 const msgEventCallback = {
-    message: function(msg) {
-        let content = msg.text;
+  message: function (msg) {
+    const content = msg.text;
 
-        // Discard if no message content or messages without prefix
-        if (!content || !content.startsWith(CONFIG.prefix)) return;
+    // Discard if no message content or messages without prefix
+    if (!content || !content.startsWith(CONFIG.prefix)) return;
 
-        for (let key in COMMANDS) {
-            let cmd = COMMANDS[key];
+    for (const key in COMMANDS) {
+      const cmd = COMMANDS[key];
 
-            // Check if command regex matches
-            let match = cmd.regex.exec(content);
-            if (!match) continue;
+      // Check if command regex matches
+      const match = cmd.regex.exec(content);
+      if (!match) continue;
 
-            // Perform relevant action
-            if (cmd.admin) {
-                // Admin only
-                if (CONFIG.admins.includes(msg.user)) {
-                    cmd.action(msg, match);
-                } else {
-                    web.chat.postMessage({
-                        channel: msg.channel,
-                        text: "> *You do not have permission to execute this command. Please contact your administrator to request access.*",
-                    });
-                }
-            } else if (cmd.private) {
-                // DMs only
-                if (msg.channel[0] === "D") {
-                    cmd.action(msg, match);
-                } else {
-                    web.chat.postMessage({
-                        channel: msg.channel,
-                        text: "> *This command can only be performed in direct messages*",
-                    });
-                }
-            } else {
-                cmd.action(msg, match);
-            }
-
-            // Only match one commmand
-            break;
+      // Perform relevant action
+      if (cmd.admin) {
+        // Admin only
+        if (CONFIG.admins.includes(msg.user)) {
+          cmd.action(msg, match);
+        } else {
+          web.chat.postMessage({
+            channel: msg.channel,
+            text: `> *You do not have permission to execute this command. 
+                Please contact your administrator to request access.*`,
+          });
         }
-    }
-}
+      } else if (cmd.private) {
+        // DMs only
+        if (msg.channel[0] === 'D') {
+          cmd.action(msg, match);
+        } else {
+          web.chat.postMessage({
+            channel: msg.channel,
+            text: '> *This command can only be performed in direct messages*',
+          });
+        }
+      } else {
+        cmd.action(msg, match);
+      }
 
-rtm.on('message', function(data) {
-    let cb = msgEventCallback[data.type];
-    if (cb) cb(data); 
+      // Only match one commmand
+      break;
+    }
+  },
+};
+
+rtm.on('message', function (data) {
+  const cb = msgEventCallback[data.type];
+  if (cb) cb(data);
 });
 
 rtm.start();
