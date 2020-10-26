97cc51e9565f1ba7bb0f433f90001e6364c6d52e
KPal81 <kpal81xd@gmail.com>
2020-10-26 16:03:40 +0000

    Added localhost scheme for local debugging

--- index.js
+++ index.js
@@ -18,6 +18,12 @@ rtm.on('ready', function () {
   console.log('============================');
   console.log(`Maia ${CONFIG.DEBUG ? "(BETA)" : ""} is ONLINE`);
   console.log('============================');
+
+  //TODO: Change id to use email instead of compound key
+  // web.users.info({
+  //   token: CONFIG.BOT_TOKEN, 
+  //   user: "U01CEG3V7B7"
+  // }).then(console.log);
 });
 
 
@@ -28,10 +34,11 @@ const msgEventCallback = {
 
     // Discard if no message content or messages without prefix
     if (!content || !content.startsWith(CONFIG.prefix)) return;
-
+  
 
     for (const key in COMMANDS) {
       const cmd = COMMANDS[key];
+
 
       // Check if command regex matches
       const match = cmd.regex ? cmd.regex.exec(content) : cmd.search(content);
