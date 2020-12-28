77093cd5242b2160e1f599c3a261ebb194788286
KPal81 <kpal81xd@gmail.com>
2020-12-28 21:07:45 +0000

    Fixed JSON bug in request

--- config.js
+++ config.js
@@ -7,6 +7,6 @@ module.exports = {
     DEBUG,
     BOT_TOKEN: DEBUG ? process.env.MAIA_BETA_BOT_TOKEN : process.env.MAIA_BOT_TOKEN,
 
-    // serverURL:  'https://maia-server.herokuapp.com',
-    serverURL:  DEBUG ? 'http://localhost:3000' : 'https://maia-server.herokuapp.com',
+    serverURL:  'https://maia-server.herokuapp.com',
+    // serverURL:  DEBUG ? 'http://localhost:3000' : 'https://maia-server.herokuapp.com',
 };
